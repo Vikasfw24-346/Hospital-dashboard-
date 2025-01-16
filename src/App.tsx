8eca18d04631204a9f3afaa497c5a81ceb752a5b
virajkumar <virajkumar12345@gmail.com>
2025-01-16 19:17:23 +0530

    website now responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
 import Navbar from './components/Navbar';
+import Helmet from 'react-helmet';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar/>
-      <Dashboard/>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
+      <Navbar />
+      <Dashboard />
     </div>
   );
 }
